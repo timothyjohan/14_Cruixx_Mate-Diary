67dff2b9a75dbf3a45b1e2adc5a99bdf076d79ae
lthreen <timothyjohan11@gmail.com>
2024-04-27 00:00:24 +0700

    Base Color and Navbar
    template kurang footer

--- mate-diary/src/pages/Home.jsx
+++ mate-diary/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 export default function Home(){
     return(
         <>
+            <Navbar />
             <h1 className="">hello home</h1>
+            <Outlet />
         </>
     )
 }
