d2bce47337724e7dfb7b238bbe646982c5c0cb56
lthreen <timothyjohan11@gmail.com>
2024-04-27 00:27:05 +0700

    Footer

--- mate-diary/src/pages/Home.jsx
+++ mate-diary/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home(){
     return(
         <>
             <Navbar />
             <h1 className="">hello home</h1>
-            <Outlet />
+            <div className="min-h-screen scrollbar scrollbar-thin">
+                <Outlet />
+
+            </div>
+
+            <Footer />
         </>
     )
 }
