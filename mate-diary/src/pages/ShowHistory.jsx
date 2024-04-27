ab838e25518737f564ddecaa8bd31eea57f5a333
lthreen <timothyjohan11@gmail.com>
2024-04-28 02:39:38 +0700

    dynamic fix history

--- mate-diary/src/pages/ShowHistory.jsx
+++ mate-diary/src/pages/ShowHistory.jsx
@@ -1,20 +1,35 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { emitToast } from "../utility/toast/toast";
 import axios from 'axios'
+import { useCookies } from "react-cookie";
 
 export default function AddUser() {
     const {register, handleSubmit} = useForm()
       const [message, setMessage] = useState(null)
       const [success, setSuccess] = useState(null)
       const [history, setHistory] = useState([])
+      const [cookies, setCookie, removeCookie] = useCookies(['currentUser']);
+      const [currentUser, setCurrentUser] = useState(null)
+      const navigate = useNavigate()
   
+      const fetchLogged = async ()  =>{
+          const data = {
+              username: cookies.currentUser
+          }
+          const result = await axios.post(`http://localhost:3000/user?username=${cookies.currentUser}`)
+          setCurrentUser(result.data.msg)
+      }
+  
+      useEffect(()=>{
+          fetchLogged()
+      },[])
   
       const fetchHistory = async () => {
-        const listHistory = await axios.get(`http://localhost:3000/history?username=buse1&password=buse123`)
-        const hewan = await axios.get(`http://localhost:3000/history?username=buse1&password=buse123`)
+        const listHistory = await axios.get(`http://localhost:3000/history?username=${currentUser.username}&password=${currentUser.password}`)
+        const hewan = await axios.get(`http://localhost:3000/history?username=${currentUser.username}&password=${currentUser.password}`)
         setHistory(listHistory.data.msg)
         setMessage(null)
     }
@@ -22,14 +37,18 @@ export default function AddUser() {
     useEffect(() => {
         fetchHistory()
         // alert(JSON.stringify(history))
-    }, [])
+    }, [currentUser])
   
     return (
       
         <div className="items-center justify-center">
+            <div>
+                <h1 className="text-xl mb-16 font-bold">Mating Sessions</h1>
+                <button onClick={()=> navigate('add')} className="transition-all px-4 py-2 text-white bg-[#B5C18E] hover:bg-[#DEAC80] rounded-xl my-auto">Add Animal</button>
+            </div>
         {
             history.map((data, index) => (
-                <div key={index} className="flex items-center justify-between w-full p-4 my-2 bg-white rounded shadow-lg">
+                <div key={index} className="flex items-center justify-between w-full text-white p-4 my-2 bg-[#DEAC80] rounded-xl shadow-lg">
                 <div className="grid grid-flow-row-dense grid-cols-6 grid-rows-1 pl-5 text-center space-x-4 w-full">
                     <div className="pt-2">{(data.id_h_kawin)}</div>
                     <div className="pt-2">{(data.user)}</div>
@@ -37,7 +56,7 @@ export default function AddUser() {
                     <div className="pt-2">{(data.animal_fem)}</div>
                     <div className="pt-2">{(data.status)}</div>
                     <div className="pt-auto">
-                        <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 my-auto">Detail</button>
+                        <button className="transition-all px-4 py-2 text-white bg-[#B5C18E] hover:bg-[#F7DCB9] hover:text-gray-800 rounded-xl my-auto">Detail</button>
                     </div>
                 </div>
                 </div>
