6b82c8ebe8811ddd17230c1cff76cfc245e64c4a
lthreen <timothyjohan11@gmail.com>
2024-04-27 17:10:19 +0700

    Color readjustment

--- mate-diary/src/pages/Login.jsx
+++ mate-diary/src/pages/Login.jsx
@@ -28,20 +28,20 @@ export default function Login(){
 
     return(
         <>
-            <div class="flex flex-col items-center justify-center px-6" style={{marginBottom: "64px"}}>
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700">
+            <div class="flex flex-col items-center justify-center px-6 text-white"  style={{marginBottom: "64px"} }>
+                <div class="w-full bg-[#B99470] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+                        <h1 class="text-xl  font-bold leading-tight tracking-tight md:text-2xl">
                             Sign in to your account
                         </h1>
                         <form onSubmit={handleSubmit(signIn)}>
                             <div className="my-2">
                                 <label for="username" class="block mb-2 text-sm font-medium">Your username</label>
-                                <input {...register("username")} type="text" name="username" id="username" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Username" required="" />
+                                <input {...register("username")} type="text" name="username" id="username" class="text-white bg-[#F7DCB9] text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Username" required="" />
                             </div>
                             <div className="my-2">
                                 <label for="password" class="block mb-2 text-sm font-medium">Password</label>
-                                <input {...register("password")} type="password" name="password" id="password" placeholder="••••••••" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <input {...register("password")} type="password" name="password" id="password" placeholder="••••••••" class="text-white bg-[#F7DCB9] text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <div class="flex items-center justify-between">
                                 <div class="flex items-start">
@@ -49,7 +49,7 @@ export default function Login(){
                                         <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
                                     </div>
                                     <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        <label for="remember" class="text-gray-200">Remember me</label>
                                     </div>
                                 </div>
                                 <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
@@ -61,9 +61,9 @@ export default function Login(){
                                 {
                                     success ? <div className="w-full bg-green-400 p-1.5 pl-3 text-white">{success}</div> : null
                                 }
-                                <button type="submit" class="w-full mt-2 text-white bg-black hover:bg-black-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <Link to={"/Register"}><a class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a></Link>
+                                <button type="submit" class="w-full mt-2 text-white bg-[#DEAC80] hover:bg-[#B5C18E] focus:ring-4 focus:outline-none focus:ring-primary-00 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                <p class="text-sm font-light text-gray-200">
+                                    Don’t have an account yet? <Link to={"/Register"}><a class="font-medium text-primary-300 hover:underline dark:text-primary-300">Sign up</a></Link>
                                 </p>
                             </div>
                         </form>
--- mate-diary/src/pages/Register.jsx
+++ mate-diary/src/pages/Register.jsx
@@ -32,8 +32,8 @@ export default function Register(){
 
     return(
         <>
-            <div class="flex flex-col items-center justify-center px-6" style={{marginBottom: "64px"}}>
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700">
+            <div class="flex flex-col items-center text-gray-100 justify-center px-6" style={{marginBottom: "64px"}}>
+                <div class="w-full bg-[#B99470] rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl">
                             Sign up to your account
@@ -41,32 +41,32 @@ export default function Register(){
                         <form onSubmit={handleSubmit(signUp)}>
                             <div className="my-3">
                                 <label for="email" class="block mb-2 text-sm font-medium">Your email</label>
-                                <input {...register("email")} type="email" name="email" id="email" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="youremail@mail.com" required="" />
+                                <input {...register("email")} type="email" name="email" id="email" class="text-black bg-[#F7DCB9]  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="youremail@mail.com" required="" />
                             </div>
                             <div className="my-3">
                                 <label for="username" class="block mb-2 text-sm font-medium">Your username</label>
-                                <input {...register("username")} type="text" name="username" id="username" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Username" required="" />
+                                <input {...register("username")} type="text" name="username" id="username" class="text-black bg-gray-50  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#F7DCB9] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Username" required="" />
                             </div>
                             <div className="my-3">
                                 <label for="nickname" class="block mb-2 text-sm font-medium">Your nickname</label>
-                                <input {...register("nickname")} type="text" name="nickname" id="nickname" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Nickname" required="" />
+                                <input {...register("nickname")} type="text" name="nickname" id="nickname" class="text-black bg-gray-50  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#F7DCB9] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Nickname" required="" />
                             </div>
                             <div className="my-3">
                                 <label for="password" class="block mb-2 text-sm font-medium">Password</label>
-                                <input {...register("password")} type="password" name="password" id="password" placeholder="••••••••" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <input {...register("password")} type="password" name="password" id="password" placeholder="••••••••" class="text-black bg-gray-50  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#F7DCB9] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <div className="my-3">
                                 <label for="confirm_password" class="block mb-2 text-sm font-medium">Confirm Password</label>
-                                <input {...register("confirm_password")} type="password" name="confirm_password" id="confirm_password" placeholder="••••••••" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <input {...register("confirm_password")} type="password" name="confirm_password" id="confirm_password" placeholder="••••••••" class="text-black bg-gray-50  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#F7DCB9] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <div className="my-3">
                                 <label for="company_name" class="block mb-2 text-sm font-medium">Company Name</label>
-                                <input {...register("company_name")} type="text" name="company_name" id="company_name" class="text-white bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Company Name" required="" />
+                                <input {...register("company_name")} type="text" name="company_name" id="company_name" class="text-black bg-gray-50  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#F7DCB9] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Company Name" required="" />
                             </div>
                             <div class="flex items-center justify-between">
                                 <div class="flex items-start">
                                     <div class="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4  rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-[#F7DCB9] dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
                                     </div>
                                     <div class="ml-3 text-sm">
                                         <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
@@ -81,8 +81,8 @@ export default function Register(){
                                 {
                                     success ? <div className="w-full bg-green-400 p-1.5 pl-3 text-white">{success}</div> : null
                                 }
-                                <button type="submit" class="w-full mt-2 text-white bg-black hover:bg-black-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                <button type="submit" class="w-full mt-2 text-white bg-[#DEAC80] hover:bg-[#B5C18E] focus:ring-4 focus:outline-none focus:ring-primary-00 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
+                                <p class="text-sm font-light text-gray-500 dark:text-gray-200">
                                     Already have account ? <Link to={"/Login"}><a class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a></Link>
                                 </p>
                             </div>
