000ba9a4fa7095adac97b16bdf1db41bb9af22aa
lthreen <timothyjohan11@gmail.com>
2024-04-28 00:40:30 +0700

    color fix

--- mate-diary/src/pages/MyAnimals.jsx
+++ mate-diary/src/pages/MyAnimals.jsx
@@ -41,9 +41,20 @@ export default function MyAnimals(){
                     <>
                         <h1 className="text-xl mb-16">My Animals</h1>
                         {
-                            animals.map((element)=>{
-                                return <AnimalItems {...element} />
-                            })
+                            animals.map((data, index) => (
+                                <div key={index} className="flex items-center justify-between w-full text-white p-4 my-2 bg-[#DEAC80] rounded-xl shadow-lg">
+                                <div className="grid grid-flow-row-dense grid-cols-6 grid-rows-1 pl-5 text-center space-x-4 w-full">
+                                    <div className="pt-2">{(data.kode_hewan)}</div>
+                                    <div className="pt-2">{(data.nama_hewan)}</div>
+                                    <div className="pt-2">{(data.nama_panggilan)}</div>
+                                    <div className="pt-2">{(data.gender)}</div>
+                                    <div className="pt-2"></div>
+                                    <div className="pt-auto">
+                                        <button className="px-4 py-2 text-white  bg-[#B5C18E] hover:bg-[#F7DCB9] hover:text-gray-800 rounded-xl my-auto">Detail</button>
+                                    </div>
+                                </div>
+                                </div>
+                            ))
                         }
                     </>
                     :
--- mate-diary/src/pages/ShowHistory.jsx
+++ mate-diary/src/pages/ShowHistory.jsx
@@ -27,20 +27,22 @@ export default function AddUser() {
     return (
       
         <div className="items-center justify-center">
-        {history.map((data, index) => (
-            <div key={index} className="flex items-center justify-between w-full p-4 my-2 bg-white rounded shadow-lg">
-            <div className="grid grid-flow-row-dense grid-cols-6 grid-rows-1 pl-5 text-center space-x-4 w-full">
-                <div className="pt-2">{(data.id_h_kawin)}</div>
-                <div className="pt-2">{(data.user)}</div>
-                <div className="pt-2">{(data.animal_male)}</div>
-                <div className="pt-2">{(data.animal_fem)}</div>
-                <div className="pt-2">{(data.status)}</div>
-                <div className="pt-auto">
-                    <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 my-auto">Detail</button>
+        {
+            history.map((data, index) => (
+                <div key={index} className="flex items-center justify-between w-full p-4 my-2 bg-white rounded shadow-lg">
+                <div className="grid grid-flow-row-dense grid-cols-6 grid-rows-1 pl-5 text-center space-x-4 w-full">
+                    <div className="pt-2">{(data.id_h_kawin)}</div>
+                    <div className="pt-2">{(data.user)}</div>
+                    <div className="pt-2">{(data.animal_male)}</div>
+                    <div className="pt-2">{(data.animal_fem)}</div>
+                    <div className="pt-2">{(data.status)}</div>
+                    <div className="pt-auto">
+                        <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 my-auto">Detail</button>
+                    </div>
                 </div>
-            </div>
-            </div>
-        ))}
+                </div>
+            ))
+        }
         </div>
     
     );
