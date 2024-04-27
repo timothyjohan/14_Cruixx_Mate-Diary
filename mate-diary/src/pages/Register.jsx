c1cde3c6caa0110c57c1e3bafa8a5ebcc7014d74
lthreen <timothyjohan11@gmail.com>
2024-04-27 14:24:49 +0700

    fix

--- mate-diary/src/pages/Register.jsx
+++ mate-diary/src/pages/Register.jsx
@@ -27,7 +27,6 @@ export default function Home(){
 
     return(
         <>
-            <Navbar />
             <div class="flex flex-col items-center justify-center px-6" style={{marginBottom: "64px"}}>
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -72,7 +71,6 @@ export default function Home(){
                     </div>
                 </div>
             </div>
-            <Footer />
         </>
     )
 }
