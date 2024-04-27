28801e0f20dcffab7ac823dc96e671d361047fa3
hansp17 <130662485+hansp17@users.noreply.github.com>
2024-04-27 15:58:14 +0700

    tampilan form  addUser revisi

--- mate-diary/src/pages/AddUser.jsx
+++ mate-diary/src/pages/AddUser.jsx
@@ -4,7 +4,6 @@ export default function AddUser() {
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [role, setRole] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,12 +32,12 @@ export default function AddUser() {
               <label htmlFor="username" className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">Username</label>
             </div>
             <div className="md:w-2/3">
-              <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="username" name="username" onChange={e => setName(e.target.value)} />
+              <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="username" name="username" onChange={e => setUsername(e.target.value)} />
             </div>
             </div>
             <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
-            <label htmlFor="password" className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">Password</label>            </div>
+            <label htmlFor="password" className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">Password</label></div>
             <div className="md:w-2/3">
             <input type="password" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="password" name="password" onChange={e => setPassword(e.target.value)} />
             </div>
