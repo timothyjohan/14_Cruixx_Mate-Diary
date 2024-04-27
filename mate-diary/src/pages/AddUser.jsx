389a8efc07c03a343b5c528a5a35ab8921d9d46c
hansp17 <130662485+hansp17@users.noreply.github.com>
2024-04-27 21:56:19 +0700

    addUser axios

--- mate-diary/src/pages/AddUser.jsx
+++ mate-diary/src/pages/AddUser.jsx
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { emitToast } from "../utility/toast/toast";
+import axios from 'axios'
 
 export default function AddUser() {
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const {register, handleSubmit} = useForm()
+    const [message, setMessage] = useState(null)
+    const [success, setSuccess] = useState(null)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission
-  };
+
+    const add = async (data, e) => {
+      setMessage(null)
+      try {
+          if(!data.name){
+            setMessage("Name required!")
+            return
+          }
+          else if(!data.username){
+            setMessage("Username required!")
+            return
+          }
+          else if(!data.password){
+              setMessage("Password required!")
+              return
+          } else {
+              await axios.post(`http://localhost:3000/karyawan`, data)
+              emitToast(`${data.username} Register new user succesfully !`, "success")
+          }
+          e.target.clear()
+      } catch(e) {
+          if(e.response.data.msg) {
+              emitToast(e.response.data.msg, "error")
+          } else {
+              emitToast(e.response.data, "error")
+          } 
+      }
+  }
 
   return (
     <section className="min-h-screen">
@@ -18,13 +47,13 @@ export default function AddUser() {
                   <h1 className="text-xl font-bold leading-tight text-[#F7DCB9] tracking-tight text-gray-900 md:text-2xl dark:text-white">
                       Add User
                   </h1>
-                  <form onSubmit={handleSubmit} className="w-full max-w-sm">
+                  <form onSubmit={handleSubmit(add)} className="w-full max-w-sm">
           <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
               <label htmlFor="name" className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">Nama</label>
             </div>
             <div className="md:w-2/3">
-              <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="name" name="name" onChange={e => setName(e.target.value)} />
+              <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="name" name="name" {...register("name")} />
             </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -32,14 +61,14 @@ export default function AddUser() {
               <label htmlFor="username" className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">Username</label>
             </div>
             <div className="md:w-2/3">
-              <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="username" name="username" onChange={e => setUsername(e.target.value)} />
+              <input type="text" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="username" name="username" {...register("username")} />
             </div>
             </div>
             <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
             <label htmlFor="password" className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">Password</label></div>
             <div className="md:w-2/3">
-            <input type="password" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="password" name="password" onChange={e => setPassword(e.target.value)} />
+            <input type="password" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#B5C18E]" id="password" name="password" {...register("password")} />
             </div>
             </div>
             <div className="md:flex md:items-center mb-6">
